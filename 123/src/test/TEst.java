3cc2ba43f30f56f8fa74bb500f91e92cc6cfdef1
Peter <Peter@Peter-PC>
2016-04-14 14:12:19 +0800

    first commit

--- 123/src/test/TEst.java
+++ 123/src/test/TEst.java
@@ -1,10 +1,10 @@
 package test;
 
-public class HelloWinnie {
+public class TEst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Brian");
+		System.out.println(1);
 	}
 
 }
