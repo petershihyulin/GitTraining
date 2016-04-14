c6f2c3635dd37f0dd36f569070ba51b7efa4fa97
Pos <Pos@Brian-PC>
2016-04-14 14:31:21 +0800

    QQ

--- 123/src/test/TEst.java
+++ 123/src/test/TEst.java
@@ -4,7 +4,7 @@ public class TEst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("eeeeeeeee");
+		System.out.println("hello ggg");
 	}
 
 }
